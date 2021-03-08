b840cc48c91f5761c604ced13913aa7e122cfedf
Stephanie Leazer <saleazer82@gmail.com>
2021-03-07 19:58:32 -0600

    Refactoring to use arrow functions.

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const chalk = require('chalk')  // makes CLI output 'pretty'
-const yargs = require('yargs')  // parses CLI input for easy manipulation
-const notes = require('./notes.js')  // imports functions for the app
-yargs.version('1.1.0')  //custom yargs version
+const chalk = require('chalk')          // makes CLI output 'pretty'
+const yargs = require('yargs')          // parses CLI input for easy manipulation
+const notes = require('./notes.js')     // imports functions for the app
+yargs.version('1.1.0')                  // custom yargs version
 
 
 //create add command
 yargs.command({
-    command: 'add',   // command to use on the CLI to initiate
-    describe: 'Add a new note',  // description of what it does for the CLI help command
-    builder: {  // builds what arguments it will take
+    command: 'add',                     // command to use on the CLI to initiate
+    describe: 'Add a new note',         // description of what it does for the CLI help command
+    builder: {                          // builds what arguments it will take
         title: {
-            describe: 'Note title',  // description of what it does for the CLI help command
-            demandOption: true,  // makes it required
-            type: 'string'  // sets the data type
+            describe: 'Note title',     // description of what it does for the CLI help command
+            demandOption: true,         // makes it required
+            type: 'string'              // sets the data type
         },
         body: {
             describe: 'Note body',  
@@ -20,15 +20,15 @@ yargs.command({
             type: 'string'  
         }
     },
-    handler: function(argv) {  // handles the input after submit
-        notes.addNote(argv.title, argv.body)  // runs add method with input provided
+    handler(argv) {                             // handles the input after submit
+        notes.addNote(argv.title, argv.body)    // runs add method with input provided
     }
 })
 
 // create remove command
 yargs.command({
-    command: 'remove',   // command to use on the CLI to initiate
-    describe: 'Remove a note',   // description of what it does for the CLI help command
+    command: 'remove',                  // command to use on the CLI to initiate
+    describe: 'Remove a note',          // description of what it does for the CLI help command
     builder: {   
         title: {
             describe: 'Note title',  
@@ -36,30 +36,35 @@ yargs.command({
             type: 'string'
         }
     },
-    handler: function(argv) {  
-        notes.removeNote(argv.title)   // runs the removeNote method with input provided
+    handler(argv) {  
+        notes.removeNote(argv.title)    // runs the removeNote method with input provided
     }
 })
 
 // create list command
 yargs.command({
-    command: 'list',  // command to use on the CLI to initiate
-    describe: 'List the notes',  // description of what it does for the CLI help command
-    handler: function() {
-       console.log(notes.listNotes())  // displays the list returned by the function
+    command: 'list',                    // command to use on the CLI to initiate
+    describe: 'List the notes',         // description of what it does for the CLI help command
+    handler() {
+       notes.listNotes()                // displays the list returned by the function
     }
 })
+
 // create read command
 yargs.command({
-    command: 'read',
-    describe: 'Read a note',
-    handler: function() {
-        console.log('Read this note')
+    command: 'read',                    // command to use on the CLI to initiate
+    describe: 'Read a note',            // description of what it does for the CLI help command
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)      // returns the requested note
     }
 })
-
-// add, remove, read, list
-
 
 //  command to parse the input information into manipulatable data
 yargs.parse()
